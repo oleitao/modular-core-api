af9b31fddc2f1ca7e68e878cbccc69aeb3af80ba
olivleitao <olivleitao@gmail.com>
2024-02-29 13:09:10 +0000

    frontend with (angular) not tested

--- frontend_project/ClientApp/src/app/@core/services/interfaces/user/auth.interface.ts
+++
@@ -1,6 +0,0 @@
-import { LoginModel } from "src/app/@core/models/user/user.model";
-
-export abstract class AuthenticationInterface {
-  abstract login(credentials: LoginModel): Promise<boolean>;
-  abstract logout(): void;
-}
